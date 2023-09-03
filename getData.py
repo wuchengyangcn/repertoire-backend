0ef8fb7640e5e488d9558d1b3faf0cf59f0b6473
Effie Zhang <yzhang77@email.wm.edu>
2023-09-03 00:21:25 -0400

    add sheet2

--- getData.py
+++ getData.py
@@ -2,6 +2,7 @@ import gspread
 import pandas as pd
 import json
 from oauth2client.service_account import ServiceAccountCredentials
+from collections import OrderedDict
 
 class jsonData:
     file = ""
@@ -15,14 +16,18 @@ class jsonData:
 
     def getData(self, filename): 
         workbook = jsonData.file.open(filename)
-        sheet = workbook.sheet1
+        sheet1 = workbook.sheet1
+        sheet2 = workbook.get_worksheet(1)
+        sheet3 = workbook.get_worksheet(2)
 
         # print(sheet.row_values(2))
-        df = pd.DataFrame(sheet.get_all_records())
+        df1 = pd.DataFrame(sheet1.get_all_records())
+        df2 = pd.DataFrame(sheet2.get_all_records())
+        df3 = pd.DataFrame(sheet3.get_all_records())
         content = []
 
         # 按照performer列进行分组
-        grouped = df.groupby('performer')
+        grouped = df1.groupby('performer')
 
         # 遍历每个分组
         for performer, group_df in grouped:
@@ -35,6 +40,16 @@ class jsonData:
                 pieces.append(piece_dict)
             performer_dict = {"performer": performer, "pieces": pieces}
             content.append(performer_dict)
+        
+        front_dict = OrderedDict()
+        row2 = df2.iloc[0] 
+        front_dict['title'] = row2[0]
+        front_dict['subtitle'] = row2[1]
+        front_dict['time'] = row2[2]
+        front_dict['location'] = row2[3]
+        front_dict['address'] = row2[4]
+        front_dict['background'] = row2[5]
+        front_dict_json = json.dumps(front_dict)
 
         music_dict = {
             "license notice": [
@@ -43,20 +58,17 @@ class jsonData:
             "This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.",
             "You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>."
             ],
-            "front": {
-            "title": "A PIANO RECITAL",
-            "subtitle": "featuring students of Miranda Shum",
-            "time": "SUNDAY, APRIL 23, 2023 | 2:00 PM",
-            "location": "Tateuchi Hall, Community School of Music and Arts",
-            "address": "230 San Antonio Circle, Mountain View CA 94040",
-            "background": "https://cdn.pixabay.com/photo/2022/11/28/18/58/piano-7622920_1280.jpg"
-            },
+            "front": front_dict_json,
         "content": content,
         "back": [],
         }
+        print(music_dict)
 
         data = json.dumps(music_dict)
         return data
+
+# test1 = jsonData()
+# test1.getData("Music-menu")
 
     
 
