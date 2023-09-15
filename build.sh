0e537c537484e2de27a0bfe82238b4ad4c8a1454
Chengyang Wu <wuchengyang.cn@gmail.com>
2023-09-15 05:43:35 -0500

    restore output

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-sudo docker build -q -t musicnbrain/repertoire-backend .
-sudo docker run -q -d -p 5001:5001 --name repertoire-backend musicnbrain/repertoire-backend
+sudo docker build -t musicnbrain/repertoire-backend .
+sudo docker run -d -p 5001:5001 --name repertoire-backend musicnbrain/repertoire-backend
