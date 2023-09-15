674a846f6b081c70ab5e05002d6543dac89839d6
Chengyang Wu <wuchengyang.cn@gmail.com>
2023-09-15 04:32:37 -0500

    modify build script

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-docker build -t musicnbrain/repertoire-backend .
-docker run -d -p 5001:5001 --name repertoire-backend musicnbrain/repertoire-backend
+sudo docker build -t musicnbrain/repertoire-backend .
+sudo docker run -d -p 5001:5001 --name repertoire-backend musicnbrain/repertoire-backend
