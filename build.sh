db325ec35ccca4f0c60338dad2bc74925fac9575
Chengyang Wu <wuchengyang.cn@gmail.com>
2023-09-15 05:51:51 -0500

    add flag

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-sudo docker build -t musicnbrain/repertoire-backend .
+sudo docker build -q -t musicnbrain/repertoire-backend .
 sudo docker run -d -p 5001:5001 --name repertoire-backend musicnbrain/repertoire-backend
